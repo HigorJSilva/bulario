e2b2dfd1acaf0d8959ec4522f9789347e96d7927
Higor Silva <higor.junio@hotmail.com>
2023-04-25 14:08:47 -0300

    fix: better order swagger docs

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Response } from 'express'
 import medicinesRoutes from '@modules/medicines/infra/http/routes'
 import leafletsRoutes from '@modules/leaflets/infra/http/routes'
 import usersRoutes from '@modules/users/infra/http/routes'
@@ -12,6 +12,10 @@ routes.use('/medicines', medicinesRoutes)
 routes.use('/leaflets', leafletsRoutes)
 routes.use('/users', usersRoutes)
 routes.use('/medications', medicationRoutes)
-routes.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+routes.get('/', function (_, res: Response) {
+  res.redirect('/api')
+})
+routes.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customSiteTitle: 'Bulario Api' }))
 
 export default routes
