1c2c48544abb31d48ecf256f6a5c7d9a59450e4b
Higor Silva <higor.junio@hotmail.com>
2023-05-13 10:56:22 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import errorHandler from './middlewares/error_handler'
 import '@shared/helpers/container'
 import routes from './routes'
+import cors from 'cors'
 
 const application = express()
 
 application.use(express.json())
+application.use(cors)
 
 application.use(routes)
 application.use(errorHandler)
