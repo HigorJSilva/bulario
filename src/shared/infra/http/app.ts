0f8853c2decc3ffc7368d395fd5f26cf6172ff6f
Higor Silva <higor.junio@hotmail.com>
2023-05-13 11:14:33 -0300

    feat: add cors origin all

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,7 +7,10 @@ import cors from 'cors'
 const application = express()
 
 application.use(express.json())
-application.use(cors)
+application.use(cors({
+  origin: '*',
+  credentials: true
+}))
 
 application.use(routes)
 application.use(errorHandler)
